f18a83f780aeb613e2297adb52d32db74426640d
Dan Nevarez <dnev.dev@gmail.com>
2017-01-10 14:51:15 -0700

    begin setup of server.js file

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+var config = require('../config.json')
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+// app.use(express.static('public'));
+
+var port = config.port;
+
+
+// endpoints
+
+app.listen(port, function() {
+    console.log("Started server on port ", port);
+});
